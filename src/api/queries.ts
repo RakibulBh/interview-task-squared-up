56777f934a208b7013722cfa81e5f00403247e6e
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-12-11 20:01:20 +0000

    add: percentages of service desk types

---
+++ src/api/queries.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { Request, Response } from "express";
+import { SampleData } from "./types";
+
+const DATA_URL =
+    "https://sampleapi.squaredup.com/integrations/v1/service-desk?datapoints=500";
+
+export const GET = async (req: Request, res: Response) => {
+    try {
+        const { data } = await axios.get<SampleData>(DATA_URL);
+        const count = data.results.length;
+        const problems = data.results.filter(
+            (data) => data.type.toLowerCase() == "problem"
+        );
+        const questions = data.results.filter(
+            (data) => data.type.toLowerCase() == "question"
+        );
+        const issues = data.results.filter(
+            (data) => data.type.toLowerCase() == "task"
+        );
+
+        res.send({
+            problemsPercentage: Math.round((problems.length / count) * 100),
+            questionsPercentage: Math.round((questions.length / count) * 100),
+            issuesPercentage: Math.round((issues.length / count) * 100),
+        });
+    } catch (e: unknown) {
+        res.status(500);
+        res.send(e);
+    }
+};
